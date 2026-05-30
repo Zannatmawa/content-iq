0c8a3598c87c884e96ba4b3d86311d3b0e487e12
zannatul mawa <zannatulmawa38@gmail.com>
2026-05-30 19:27:56 +0600

    creating folders

---
+++ src/lib/prisma/index.ts
@@ -0,0 +1,12 @@
+// 5. Create src/lib/prisma/index.ts with this content:
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ["query"],
+    });

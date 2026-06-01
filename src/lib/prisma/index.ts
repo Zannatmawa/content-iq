d2df2d76eb498dbac6190cb7dd99cfe7141313bc
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-01 20:41:34 +0600

    resolving issues

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -11,4 +11,4 @@ export const prisma =
         log: ["query"],
     });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;

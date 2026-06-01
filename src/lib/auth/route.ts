793f082056182a63e4db3587c00f98ddd031b6c0
zannatul mawa <zannatulmawa38@gmail.com>
2026-06-01 20:32:50 +0600

    resolving issues

--- src/lib/auth/route.ts
+++ src/lib/auth/route.ts
@@ -3,8 +3,9 @@
 // app/api/auth/[...nextauth]/route.ts
 
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
+import { authConfig } from "./auth.config";
+// import { authOptions } from "@/lib/auth";
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authConfig);
 
 export { handler as GET, handler as POST };
